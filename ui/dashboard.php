56927a4608290a34e065c08cf770a2d7ad74b3ed
grindo26 <30309493+grindo26@users.noreply.github.com>
2017-09-07 02:11:08 +0530

    Add files via upload

---
+++ ui/dashboard.php
@@ -0,0 +1,33 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Dashboard</title>
+		<link rel="stylesheet" type="text/css" href="layout.css">
+	</head>
+	<body>
+
+	<!-- nav bar -->
+	<ul>
+		<li><h4>NESTLE</h4></li>
+	
+		
+	<li><a href="#dashboard" id="item">Dashboard</a></li>
+	<li><a href="products.php">Products</a></li>
+	<li><a href="transfer.php">Transfer</a></li>
+	<li><a href="operations.php">Operations</a></li>
+	<li><a href="warehouse.php">Warehouse</a></li>
+	</ul>
+	
+	<br><br><br>
+	<!-- end of nav bar -->
+
+
+	<div class="todo">
+		
+	</div>
+	<div class=" ">
+		
+	</div>
+	
+	</body>
+	</html>
