b14fd50d69d5475ae9319afa99ef6d850e0e0b32
grindo26 <30309493+grindo26@users.noreply.github.com>
2017-09-08 02:01:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,17 +83,15 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/dashboard',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'dashboard.php')); 
-});
 
-app.get('/products',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'products.php')); 
-});
 
 
 
