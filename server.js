2138fe048e2ee2dfa4f8b7f026a4134d763e035c
grindo26 <30309493+grindo26@users.noreply.github.com>
2017-09-10 12:12:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,10 +78,12 @@ app.get('/:articleName',function(req,res){
 });
 
 var counter=0;
-app.get('/counter', fuction(req, res) {
+
+app.get('/counter', function(req, res){
     counter=counter+1;
     res.send(counter.toString());
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
