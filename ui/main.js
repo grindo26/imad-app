b42f95b31e8e9a1baa2366a24e0c9e65df19168f
grindo26 <30309493+grindo26@users.noreply.github.com>
2017-09-10 12:19:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,10 @@
-console.log('Loaded!');
-var element = document.getElementById("main-text");
-
-element.innerHTML="newValue";
+var button= document.getElementById('counter');
+button.onclick = function () {
+    
+    
+    
+    
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
