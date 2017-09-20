1a4a230eb7ac5621faa3a67f1da70e6bd5d03150
grindo26 <30309493+grindo26@users.noreply.github.com>
2017-09-20 20:56:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,14 @@ app.get('/counter', function(req, res){
     res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name', function (req, res) {
+    var name= req.query.name;
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
+});
+
 
 app.get('/:articleName',function(req,res){
     var articleName= req.params.articleName;
@@ -100,13 +108,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names=[];
-app.get('/submit-name/:name', function (req, res) {
-    var name= req.params.name;
-    names.push(name);
-    
-    res.send(JSON.stringify(names));
-});
+
 
 
 
