1d00d38e205df447ce802947b775900f81130e49
grindo26 <30309493+grindo26@users.noreply.github.com>
2017-09-20 20:46:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+//counter
 var but= document.getElementById('cou');
 but.onclick= function () {
     
@@ -18,4 +19,22 @@ but.onclick= function () {
     request.send(null);
     
     
+};
+
+
+//name
+ var nameInput = document.getElementById('name');
+ var name=nameInput.value;
+ var submit= document.getElementById('submit_btn');
+ submit.onclick = function() {
+     
+ 
+     var names= ['name1', 'name2', 'name3'];
+     var list='';
+     for (var i = 0; i < names.length; i++) {
+        list +='<li>' + names[i]+ '</li>';
+    }
+    var ul= document.getElementById('namelist');
+    ul.innerHTML = list;
+
 };
