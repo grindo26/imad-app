a7434885cf66e31518e5193773756b0d4fb472a3
grindo26 <30309493+grindo26@users.noreply.github.com>
2017-09-22 00:02:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ var htmlTemplate=
             ${heading}
         </h3>
         <div>
-            ${date}
+            ${date.toDateString()}
         </div>
         <div>
             ${content}
@@ -112,7 +112,7 @@ app.get('/submit-name', function (req, res) {
 });
 
 
-app.get('/articles/:articleName',function(req,res){
+app.get('/articles/:articleName', function(req,res){
     pool.query("SELECT * FROM article WHERE title= '" + req.params.articleName + "'", function(err, result){
       if (err){
         res.status(500).send(err.toString());
